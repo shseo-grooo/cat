6b6a3023dbbc02ad6e84b3d3dedf663d09469aaa
shseo-grooo <shseo@grooo.co.jp>
2022-02-17 08:13:58 +0900

    v4

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
+	meow := []string{"야옹~", "애옹", "냥", "샤하아아아악", "골골골골", "냐!"}
+
 	r := gin.Default()
 
 	r.GET("/meow", func(c *gin.Context) {
+
 		mode := c.Request.Header["X-Mode"]
 		mode = append(mode, "")
-		c.String(http.StatusOK, "[Cat Server v3 - %s] 야옹", mode[0])
+
+		c.String(http.StatusOK, "[Cat Server v4 - %s] %s", mode[0], meow[rand.Intn(len(meow))])
 	})
 
 	r.Run()
