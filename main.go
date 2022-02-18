5d3fab1f9e6c21b990dfda6ce59769daec048d09
shseo-grooo <shseo@grooo.co.jp>
2022-02-18 09:10:34 +0900

    v6.2

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		mode := c.Request.Header["X-Mode"]
 		mode = append(mode, "")
 
-		c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("[Cat Server v6.1 - %s] %s", mode[0], meow[rand.Intn(len(meow))])})
+		c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("[Cat Server v6.2 - %s] %s", mode[0], meow[rand.Intn(len(meow))])})
 	})
 
 	r.Run()
